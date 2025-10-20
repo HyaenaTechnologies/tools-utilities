045d2b32a7c47a0a93227f90c06da3e4801bd9e5
AbdhilahiRWabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2025-10-20 16:41:00 -0600

    Delete UpT

--- upt/src/arguments.rs
+++
@@ -1,5 +0,0 @@
-pub mod argument_tokenizer;
-
-pub mod print_help;
-
-pub mod print_version;
--- upt/src/upgrades.rs
+++
@@ -1,9 +0,0 @@
-pub mod apt_upgrade;
-
-pub mod dnf_upgrade;
-
-pub mod dnf_release;
-
-pub mod snap_refresh;
-
-pub mod ubuntu_release;
